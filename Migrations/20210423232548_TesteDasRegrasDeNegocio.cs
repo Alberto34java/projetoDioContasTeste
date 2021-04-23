dc8ec5108e432ed5d370057626d046399392fa44
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-23 20:29:01 -0300

    Criado as Migrations

---
+++ Migrations/20210423232548_TesteDasRegrasDeNegocio.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BancoLacerda_Teste.Migrations
+{
+    public partial class TesteDasRegrasDeNegocio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
